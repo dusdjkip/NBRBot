f462fc2a12c8fa6f5b1008fbe21e818d5a902161
BuildTools <unconfigured@null.spigotmc.org>
2020-05-25 12:36:21 +0200

    Update watkan.js

--- commands/watkan.js
+++ commands/watkan.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = (bot, message, args) => {
-    var opties = ["Kan helemaal niks", "kan vliegen kijk maar!", "kan vrij weinig", "kan nog net plassen", "kan zichzelf pijn doen"]
+    var opties = ["Kan helemaal niks", "kan vliegen kijk maar!", "kan vrij weinig", "kan nog net plassen", "kan zichzelf pijn doen", "Kan erg slim zijn", "Kan erg dom zijn", "Kan gewoon radio maken hoor", "Kan bij de politie werken", "Kan vuilnisman worden", "Kan succesvol worden met Tikok"]
     var optie = Math.floor(Math.random() * opties.length);
 
     message.channel.send(`${args[0]} ` + opties[optie]);
