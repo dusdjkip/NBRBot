cdfa9ed58533857a6c39bcac50fd8da247da7de2
BuildTools <unconfigured@null.spigotmc.org>
2020-06-01 22:20:07 +0200

    Update ticketclose.js

--- commands/ticketclose.js
+++ commands/ticketclose.js
@@ -17,7 +17,7 @@ module.exports.run = async(bot, message, args) => {
     // Embed Create
     var embedCreateTicket = new discord.MessageEmbed()
         .setTitle("Ticket, " + message.channel.name)
-        .setDescription(`Deze ticket is afgerond door ${member.user.username}`)
+        .setDescription("De ticket is gemarkeerd als **Compleet**.")
         .setFooter("© NBRadio 2020", "https://cloud.gmpnetwork.nl/index.php/apps/files_sharing/publicpreview/S5GjTjc8JNDoXij?x=1018&y=797&a=true&file=dec2019logo2favicon.png&scalingup=0")
         .setTimestamp();
 
